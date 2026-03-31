42175da0ef2a3677e7bba8ee0918dcf2879fedc2
Santhosh <your@gmail.com>
2026-03-31 22:39:56 +0530

    fix image config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+ /** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["image.tmdb.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+    ],
   },
+};
+
+export default nextConfig;
 };
 
 export default nextConfig;
